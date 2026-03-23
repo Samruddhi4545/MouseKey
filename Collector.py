b702e2e4c19acfb68fff18e6897d470ba3bccd4f
samruddhi <paisamruddhi4545@gmail.com>
2026-03-23 13:02:40 +0530

    data collection

--- Collector.py
+++ Collector.py
@@ -2,9 +2,7 @@ import time
 import pandas as pd
 from pynput import keyboard
 
-# Data storage
 data = []
-# Dictionary to track when a specific key was pressed
 key_press_times = {}
 last_release_time = None
 
@@ -13,14 +11,13 @@ def on_press(key):
     current_time = time.time()
     
     try:
-        key_name = key.char # Regular characters
+        key_name = key.char
     except AttributeError:
-        key_name = str(key) # Special keys (Shift, Space, etc.)
+        key_name = str(key)
 
     if key_name not in key_press_times:
         key_press_times[key_name] = current_time
         
-        # Calculate Flight Time (Time since the LAST key was released)
         flight_time = 0
         if last_release_time is not None:
             flight_time = current_time - last_release_time
@@ -40,12 +37,10 @@ def on_release(key):
         press_time = key_press_times.pop(key_name)
         dwell_time = current_time - press_time
         
-        # We record: Key, Dwell Time
         data.append({'key': key_name, 'dwell_time': dwell_time})
         last_release_time = current_time
 
     if key == keyboard.Key.esc:
-        # Stop listener and save data
         df = pd.DataFrame(data)
         df.to_csv('user_typing_data.csv', index=False)
         print("\nData saved to user_typing_data.csv. Exiting...")
